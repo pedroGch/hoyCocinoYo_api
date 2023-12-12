1faaa6ef3b40a270802bfc1400553d6972fd1ba1
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-12-12 12:58:53 -0300

    agrego validaciones para alta de receta

--- src/schemas/receta.schemas.js
+++ src/schemas/receta.schemas.js
@@ -1,13 +1,16 @@
 import yup from 'yup'
 
-const msj_requided  = 'este es obligatorio'
+const msj_nombre_requided  = 'el nombre de la receta es obligatorio'
+const msj_preparacion_requided  = 'la preparacion de la receta es obligatoria'
+const msj_ingredientes_requided  = 'los ingredientes son requeridos'
+const msj_categoria_requided  = 'la categoria de la receta es obligatori'
 const msj_min_array = 'una receta no puede tener menos de 3 ingredientes'
  
 
 export const recetaCrearSchema = yup.object({
 
-  nombre  : yup.string().required(msj_requided),
-  categoria : yup.string().required(msj_requided),
-  ingredientes : yup.array().min(3, msj_min_array).required(msj_requided),
-  preparacion : yup.string().required(msj_requided)
+  nombre  : yup.string().required(msj_nombre_requided),
+  categoria : yup.string().required(msj_categoria_requided),
+  ingredientes : yup.array().required(msj_ingredientes_requided),
+  preparacion : yup.string().required(msj_preparacion_requided)
 })
