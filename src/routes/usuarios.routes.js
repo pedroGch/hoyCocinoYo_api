13201f62bddd839982298417b08cb953dbd60eb2
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-12-14 19:38:27 -0300

    agrego las nuesvas rutas

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,14 +4,20 @@ import {valInicioSesion} from '../middlewares/login.middlewares.js'
 import {usernameExiste,emailExiste,valUsuariRegistro} from '../middlewares/usuarios.middlewares.js'
 import {iniciarSesionUsuario,registrarUsuario,desloguear} from "../controllers/usuarios.controllers.js"
 import {recetasPorUsuario} from "../controllers/recetas.controllers.js"
+import {forgotPassword} from '../controllers/forgotPassword.controllers.js'
+import {resetPassword} from '../controllers/resetPassword.controllers.js'
 
 export const routerUsuarios = express()
 
 routerUsuarios.post('/iniciar-sesion', [valInicioSesion], iniciarSesionUsuario)
+
 routerUsuarios.post('/registrar',[valUsuariRegistro],[emailExiste], [usernameExiste], registrarUsuario)
 
 routerUsuarios.post('/cerrar-sesion',[checkAuth] ,desloguear) 
+
 routerUsuarios.get('/:id/recetas', recetasPorUsuario)
 
+routerUsuarios.get('/recuperar-password', forgotPassword)
 
+routerUsuarios.post('/restituir-password/:id/:token', resetPassword)
 
