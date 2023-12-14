a1a0ad31763f51a446fa3545581ed5fe6002826b
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-12-14 19:40:31 -0300

    funcionalidad que manda mail para pass nuevo

---
+++ src/controllers/forgotPassword.controllers.js
@@ -0,0 +1,47 @@
+import nodemailer from 'nodemailer'
+import jwt from 'jsonwebtoken'
+import { obtenerPorEmail, editarUsuario } from '../services/usuarios.services.js'
+import 'dotenv/config'
+//TODO: antes de llegar acá hay que checkear que se haya ingresado un email
+export async function forgotPassword (req, res) {
+  const email =  req.body.email;
+  const usuario = await obtenerPorEmail(email)
+
+
+  if (!usuario){
+    res.status(403).send('este email no existe')
+  }
+
+  const token = jwt.sign({id: usuario._id}, process.env.SECRETKEY, {expiresIn: 3600})
+  
+  editarUsuario(usuario._id, {token_reset: token})
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: `${process.env.EMAIL_ADDRESS}`,
+      pass: `${process.env.EMAIL_PASSWORD}`,
+    }
+  })
+
+  const emailPort = process.env.PORT || 3000
+
+  const mailOptions  = {
+    from: 'un email',
+    to: `${usuario.email}`,
+    subject: 'recuperar contraseña',
+    text: `http://127.0.0.1:${emailPort}/api/v1/usuarios/restituirPassword/${usuario._id}/${token}`
+  }
+
+  transporter.sendMail(mailOptions, (err, response) => {
+    if (err){
+      console.log('Ha ocurrido: ', err);
+    }
+    else{
+      console.log('respuesta: ', response );
+      res.status(200).json('El email para la recuperacion ha sido enviado')
+    }
+  })
+
+}
+
