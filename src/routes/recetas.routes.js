b2879cd1b7cafc58f291fad6f2d208bd5b6c139d
Franco Campaiola <francocampaiola@hotmail.com>
2023-12-18 14:41:39 -0300

    Subir archivos al servidor backend

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,23 +1,46 @@
-import express from 'express'
-import {valRecetaCrear} from '../middlewares/receta.middlewares.js'
-import {checkAuth} from '../middlewares/auth.middleware.js'
-import {recetas, recetaId, nuevaReceta, editar, borrar} from "../controllers/recetas.controllers.js"
+import express from "express";
+import multer from "multer";
+import { valRecetaCrear } from "../middlewares/receta.middlewares.js";
+import { checkAuth } from "../middlewares/auth.middleware.js";
+import {
+  recetas,
+  recetaId,
+  nuevaReceta,
+  editar,
+  borrar,
+} from "../controllers/recetas.controllers.js";
 
-export const routerRecetas = express()
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export const routerRecetas = express();
 
 //retorna todas las recetas
-routerRecetas.get('/todas', recetas)
+routerRecetas.get("/todas", recetas);
 
 //crea una receta
-routerRecetas.post('/',[checkAuth],[valRecetaCrear], nuevaReceta)
+routerRecetas.post(
+  "/",
+  [checkAuth],
+  upload.single("imagen"),
+  [valRecetaCrear],
+  nuevaReceta
+);
 
 //devuelve una receta en base a su id
-routerRecetas.get('/:id', recetaId)
+routerRecetas.get("/:id", recetaId);
 
 //editar una receta
-routerRecetas.put('/:id',[checkAuth] ,editar)
+routerRecetas.put("/:id", [checkAuth], editar);
 
 //borrar una receta
-routerRecetas.delete('/:id',[checkAuth] ,borrar)
-
-
+routerRecetas.delete("/:id", [checkAuth], borrar);
